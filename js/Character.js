0eb6cc328c20f5bdfc46efab2a2ec3e3487c6fa7
Dan Russell <danr94@gmail.com>
2016-01-30 12:10:09 -0800

    Added FireSpot and Character.

---
+++ js/Character.js
@@ -0,0 +1,3 @@
+function Character(sprite) {
+    this.sprite = sprite;
+}
---
+++ js/FireSpot.js
@@ -0,0 +1,5 @@
+function FireSpot(position, flip, character) {
+    this.position = position;
+    this.flip = flip;
+    this.character = character;
+}
